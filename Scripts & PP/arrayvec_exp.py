3ca2e84a48e6a5157e429f88b11b42019d29eea8
carcalilly <97712072+carcalilly@users.noreply.github.com>
2025-02-20 01:04:26 -0500

    Add files via upload

--- Scripts & PP/arrayvec_exp.py
+++ Scripts & PP/arrayvec_exp.py
@@ -1,20 +1,27 @@
 """
 The arrays and vector representations, experimental script
+Use: python array_exp.py directory
 """
 
 #Libraries
-import numpy  as np
+import numpy as np
 import sys
 import os
+import matplotlib.pyplot as plt
+from   scipy.interpolate import interp1d, make_interp_spline   
+from   scipy.integrate   import simpson, trapezoid
+from   scipy.signal import savgol_filter
+from   scipy.ndimage import gaussian_filter1d
 
 
 
 #For the input folder that holds the AECCAR files 
 folder_path = sys.argv[1] #folder path specified in command line
 files = os.listdir(folder_path) #list of files in the folder
+elemfolder = [f for f in os.listdir(folder_path) if os.path.isdir(os.path.join(folder_path, f))]
 
 
-z_values = [] #initialize list for atomic numbers
+zvschg = [] #initialize list for atomic numbers
 vasp_file = {} #initialize dictionary for VASP file content 
 periodic_table = {"H": 1, "He": 2, "Li": 3, "Be": 4, "B": 5, "C": 6, "N": 7, "O": 8, "F": 9, "Ne": 10,
     "Na": 11, "Mg": 12, "Al": 13, "Si": 14, "P": 15, "S": 16, "Cl": 17, "Ar": 18,
@@ -30,40 +37,60 @@ periodic_table = {"H": 1, "He": 2, "Li": 3, "Be": 4, "B": 5, "C": 6, "N": 7, "O"
     "Pa": 91, "U": 92, "Np": 93, "Pu": 94, "Am": 95, "Cm": 96, "Bk": 97, "Cf": 98,
     "Es": 99, "Fm": 100} #dictionary for periodic table
 
-#NOTE: something is wrong with the code below, but I have no idea what it is!
+def calcrrho(data):
+    
+    a = float(data[1].split()[0])
+    a1=a*(np.sum([float(x)**2 for x in data[2].split()]))**0.5 
+    a2=a*(np.sum([float(x)**2 for x in data[3].split()]))**0.5
+    a3=a*(np.sum([float(x)**2 for x in data[4].split()]))**0.5
+    vol = a1*a2*a3
 
-for filename in files: #for each file in the folder
-    AEC = os.path.join(folder_path, filename) #path to the file
+    nat      = np.sum([int(x) for x in data[6].split()])
+    nx,ny,nz = [ int(x) for x in data[nat+9].split() ]
+    dx = a1/nx
+    dy = a2/ny
+    dz = a3/nz
+    dvol = dx*dy*dz
+
+    ncolums    = len(data[nat+11].split())
+    grid_lines = int(nx*ny*nz/ncolums)
+    if float(nx*ny*nz)/ncolums > grid_lines :
+        grid_lines += 1
+
+    nat_lines = int(nat/ncolums)
+    if float(nat)/ncolums > nat_lines : 
+        nat_lines  += 1
+
+    data_1D = []
+    for l in range(nat+10, nat+10+grid_lines) : 
+        for x in data[l].split() :              
+            data_1D.append(float(x))
+    data_3D = np.reshape(np.array(data_1D),(nx,ny,nz),order='F')/vol
+
+    result = np.sum(data_3D)*dvol
+    return result
+
+for elemfolder in elemfolder: #for each file in the folder
+    CHG = os.path.join(folder_path, elemfolder, "CHG") #path to the file
     try:
-        open(AEC, 'r')
-        element = (AEC[5].split('/')[0]) #pull element from line 6
-        
+        with open(CHG, 'r') as file:
+            data = file.readlines() #read lines and store data
+        line6 = data[5].strip()
+        element = (line6.split('/')[0]) #pull element from line 6
+        # print(f"Debug: element variable {element} from {elemfolder}")
+
+        tot_charge = calcrrho(data)
+
         if element in periodic_table:
             at_num = periodic_table[element] #get atomic number from the dictionary
-            z_values.append(at_num) #add atomic number to the list
+            zvschg.append((at_num, tot_charge)) #add atomic number to the list
         else: 
-            print(f"Error: Cannot read element from {filename}.")
+            print(f"Error: Cannot read element from {elemfolder}.")
     except ValueError:
-            print(f"Error: Cannot read atomic number from {filename}.")
+            print(f"Error: Cannot read atomic number from {elemfolder}.")
         
-z_values = np.array(z_values) #convert list to 1D numpy array
-
-print("Atomic Numbers: ", z_values) #print atomic numbers
-
-
-"""
-NOTE: code for extracting atomic numbers from file name
-for filename in files: #for each file in the folder
-    if '-' in filename:
-        try:
-            at_num = int(filename.split('-')[0]) #pull atomic number from the file name
-            z_values.append(at_num) #add atomic number to the list
-        except ValueError:
-            print(f"Error: {filename} does not follow naming convention.")
-        
-z_values = np.array(z_values) #convert list to 1D numpy array
-
-print("Atomic Numbers: ", z_values) #print atomic numbers
-"""
+z_totchg_arr = np.array(zvschg) #convert list to 2D numpy array
+z_totchg_arr = z_totchg_arr[z_totchg_arr[:,0].argsort()]
+print(f"Atomic Z vs. Charge \n", z_totchg_arr)
 
 
